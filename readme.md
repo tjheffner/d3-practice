7670ff926ed8dfe616d1540380bf73075869c61d
Tanner Heffner <tannerjheffner@gmail.com>
2016-02-25 21:47:24 -0800

    put the styles in the right place lol

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ learning how to use the d3 library to visualize data. how fun!
 
 # setup
 clone the repository & start a local server from that directory.
-try `python -m SimpleHTTPServer 8888 &` or `php -S localhost:8888`
+try `python -m SimpleHTTPServer 8888 &`
+or `php -S localhost:8888`
 
 then visit localhost:8888 in your browser.
--- scripts.js
+++ scripts.js
@@ -2,10 +2,20 @@ var svgW = 1000,
     svgH = 1000,
     pad = 2;
 
-var data; // a global
+var USA; // a global
 
 d3.json("states.json", function(error, json) {
   if (error) return console.warn(error);
-  data = json;
+  USA = json;
   visualizeit();
 });
+
+d3.select("body")
+  .append("p")
+  .text("is this really all it takes?")
+  .style("color", "#ff6600")
+  .style("font-size", "40pt");
+
+function visualizeit(USA) {
+  console.log(USA);
+}
